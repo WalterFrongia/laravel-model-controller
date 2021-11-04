d33cb7395ab92206d1ae72dc8697c8cdcef913e7
WalterFrongia <walter.frongia@libero.it>
2021-11-04 17:11:43 +0100

    Rappresentato db in Movie con foreach

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
     public function index(){
 
-        return view('movie');
+        $movies = Movie::all();
+        // dd($movies); //questo serve per vedere l'array, in attribute si vedono i valori
+        $data = [
+            //questo serve per passare alla view il contenuto
+            "movies" => $movies
+        ];
+        return view('movie', $data);
     }
 }
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -2,4 +2,9 @@
 
 @section("content")
     <h1>movie</h1>
+    @foreach($movies as $movie)
+        <div id="container-card">
+            <h1>{{$movie->title}}</h1>
+        </div>
+    @endforeach
 @endsection
